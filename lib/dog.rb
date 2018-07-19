35ebafb6efb32e510262c109cbc08755b891ce5e
Ramona M <r1mari@yahoo.com>
2018-07-19 19:04:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,19 @@
 class Dog
  @@all = []
+ 
  def initialize(name)
    @name = name
    
    @@all << self
   end
+  
   def dog_all
     @@all.each do {|name| print name, " "}
   end 
   end
   
-  def clear_all
+#   def clear_all
     
-end
+# end
   
 end
